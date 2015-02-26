25eb426a5f8d46cf961a88085da56cf1b3e9592a
Thomas <thomashenry@hotmail.fr>
2015-02-26 13:23:50 +0100

    suppression create-account

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
-require("smarty.class.php"); // On inclut la classe Smarty
+require("tpl/smarty.class.php"); // On inclut la classe Smarty
 
 $smarty = new Smarty();
+
+
 
 $smarty->display("index.html");
 ?>
