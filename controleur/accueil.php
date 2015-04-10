f4bdf12cacbd9b360921f73ef8cb11e90bc47d2f
Thomas <thomashenry@hotmail.fr>
2015-04-10 15:04:52 +0200

    intégration flux rss

--- controleur/accueil.php
+++ controleur/accueil.php
@@ -7,6 +7,10 @@ class Accueil
 		$template = new RainTPL();
 		$template->assign("authenticated",$_SESSION['authenticated']);
 		$template->assign("message",$msg);
+		require_once("lib/rsslib.php");
+		$url = "http://www.france24.com/fr/france/rss/";
+		$rss= RSS_Display($url, 15, false, true);
+		$template->assign("rss",$rss);
 		$html=$template->draw('vueAccueil', $return_string=true);
 		echo $html;
 	}
