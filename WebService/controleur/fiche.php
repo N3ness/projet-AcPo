c4b89fe137c933bb8b7ad27d4194b227a2f5bc96
Paul <pribierre@gmail.com>
2015-05-03 00:03:52 +0200

    mAj

--- WebService/controleur/fiche.php
+++ WebService/controleur/fiche.php
@@ -9,26 +9,42 @@ class Fiche
 		$table = $model->getPathoById($id);
 
 		//var_dump($table);
+	
+	//création d'un nouveau document
+		$xml = new DOMDocument( "1.0", "UTF-8" );
 		
-		$chaineXML = '<?xml version="1.0" encoding="UTF-8" ?>';
-		$chaineXML .= '<!DOCTYPE document SYSTEM "projet.dtd">';
-		$chaineXML .= '<?xml-stylesheet type="text/xsl" href="patho.xsl" ?>';
-		$chaineXML .= '<pathologie>';
-		$chaineXML .= '<description>'.$table[0]['Description'].'</description>';
-		$chaineXML .= '<meridien>'.$table[0]['Meridien'].'</meridien>';
-		$chaineXML .= '<symptomes>';
-		foreach($table as $sympt){
-			$chaineXML .= '<symptome>'.$sympt['Symptome'].'</symptome>';
-		}
-		$chaineXML .= '</symptomes>';
-		$chaineXML .= '</pathologie>';
+	//Indentation
+		$xml->preserveWhiteSpace = false;
+		$xml->formatOutput = true;
+
+	//Ajout doctype
+	 	$xml->schemaValidate("schema_pathos.xsd");
+		/*$xml->appendChild($doctype); */
 		
-		$dom = new DomDocument();
-		$dom->loadXML($chaineXML);
-		$dom->save('patho'.$id.'.xml');
+	//Lien vers le XSL
+		$xslt = $xml->createProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="patho.xsl"');
+		$xml->appendChild($xslt);
+		
+	//Ajoute des elements 
+		//Ajout de l'element racine
+		$patho = $xml->createElement('pathologie');
+		//Element Description
+		$desc = $xml->createElement('description',$table[0]['Description']);
+		$patho->appendChild($desc);
+		
+		$meridien = $xml->createElement('meridien',$table[0]['Meridien']);
+		$patho->appendChild($meridien);
+		
+		$sympts = $xml->createElement('symptomes');
+			foreach($table as $sympt){
+				$sympt = $xml->createElement('symptome',$sympt['Symptome']);
+				$sympts->appendChild($sympt);
+			}
+		$patho->appendChild($sympts);
+		
+		$xml->appendChild($patho);
+		$xml->save('patho'.$id.'.xml');
 		header('Location: ../patho'.$id.'.xml');
 		exit();
-		
-
 	}
 }
