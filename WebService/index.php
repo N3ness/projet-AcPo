381312aeef040602e6d535af1718c1a0863393b5
Paul <pribierre@gmail.com>
2015-05-02 20:46:26 +0200

    Debug

--- WebService/index.php
+++ WebService/index.php
@@ -2,9 +2,10 @@
 	
 	require('vue/header.html');
 	require('vue/vueAccueil.html');
-	require('controleur/fiche.php');
-	$vue = new Fiche();
-	$vue->execute($_GET['id']);
 	
+	if (isset($_GET['section']) && $_GET['section'] == 'fiche'){
+		require('controleur/fiche.php');
+		$vue = new Fiche();
+		$vue->execute($_GET['id']);
+	}
 	require('vue/footer.html');
-?>
