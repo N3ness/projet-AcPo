d1aa5c5d5c02c9fc3e23665b78c7fd37b21f7983
Paul <pribierre@gmail.com>
2015-05-02 20:34:28 +0200

    Correction

--- WebService/index.php
+++ WebService/index.php
@@ -1,9 +1,10 @@
 <?php
-	require('controleur/fiche.php');
+	
 	require('vue/header.html');
 	require('vue/vueAccueil.html');
-	
-	
+	require('controleur/fiche.php');
+	$vue = new Fiche();
+	$vue->execute($_GET['id']);
 	
 	require('vue/footer.html');
 ?>
